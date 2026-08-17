2b6e7f4bbe9f7fe4ea3b87a8be1bd89842f4c733
Sameer Shaik <23BQ1A12F3@vvit.net>
2026-08-17 14:36:35 +0530

    Update vite.config.js

--- MileStone3-backup/frontend/vite.config.js
+++ MileStone3-backup/frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://civicpulsebackend.vercel.app/',
         changeOrigin: true,
         secure: false,
       },
